8c3c163182dcd4bb9423610befadaca4a68d7f9d
KC <kc@users.noreply.github.com>
2026-06-18 10:39:31 +0800

    拼豆工具 - 支持GitHub Pages部署

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/bead-tool/',
   plugins: [
     vue(),
     Components({
